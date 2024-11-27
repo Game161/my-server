f18a3bf59e9012dbbc352638c6ffd26d50a53c5c
Game161 <nutchanonza161248@gmail.com>
2024-11-28 05:59:59 +0700

    first commit

--- src/index.js
+++ src/index.js
@@ -6,7 +6,7 @@ const port = process.env.PORT || 3000;
 const bodyParser = require("body-parser");
 const cors = require("cors");
 const productRoute = require('./routes/product.route');
-
+const customersRoute = require('./routes/customers.route');
 app.use(cors());
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
@@ -16,6 +16,7 @@ app.get("/",(req,res) =>{
 });
 
 app.use("/products", productRoute);
+app.use("/customers",customersRoute);
 
 app.listen(port, () => {
   console.log("App started at port: " + port);
